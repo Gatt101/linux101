d29fbc26aa25872e603dc2b371afce9d615f2484
gatt <gauravpatilk11@gmail.com>
2024-01-30 18:06:41 +0400

    new

---
+++ java/my.java
@@ -0,0 +1,13 @@
+import java.util.HashMap;
+
+public class my {
+    HashMap<Integer,Integer> ans;
+
+    my(){
+    
+    }
+    public static void main(String[] args) {
+        
+    }
+
+}
---
+++ java/palindrome.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class palindrome {
+
+
+     public static List<List<String>> partition(String str) {
+        // Write your code here.
+        List<List<String>> ll=new ArrayList<>();
+    
+        return ll;
+    }
+    public static void main(String[] args) {
+        
+    }
+}
